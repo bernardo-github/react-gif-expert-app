03e9a5430a448f5d4750da3ca28c8c6679064c34
bernardo-github <bernialvarez@gmail.com>
2021-04-08 15:13:19 +0200

    url con https

--- src/helpers/getGifs.jsx
+++ src/helpers/getGifs.jsx
@@ -1,7 +1,7 @@
 export  const getGifs = async( category ) => {
        
     const api_key= 'SzTGB7rnELWEXmMg3mJRybSV8IRY0CLa'
-    const url_base='http://api.giphy.com/v1/gifs/search'
+    const url_base='https://api.giphy.com/v1/gifs/search'
     const limit = '10'
     const resp = await fetch(`${ url_base }?q=${ encodeURI( category ) }&api_key=${ api_key }&limit=${ limit }`);
     const { data } = await resp.json();
